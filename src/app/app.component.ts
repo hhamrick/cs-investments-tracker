6636b54e45f4b64d924b2651fa7b5c841a0dab60
hhamrick <hhamrick@email.unc.edu>
2024-12-10 10:02:31 -0500

    Search bar, search page now shows all searched items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Observable, of } from 'rxjs';
 import { UserService } from './user/user.service';
 import { User } from './user/user.model';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, CommonModule, MatToolbar, MatButton, MatMenuModule],
+  imports: [RouterOutlet, RouterLink, CommonModule, MatToolbar, MatButton, MatMenuModule, MatInputModule, FormsModule, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'cs-investments-tracker';
   user: Observable<User | null>;
+  public searchTxt = "";
 
-  constructor(protected userService: UserService) {
+  constructor(protected router: Router, protected userService: UserService) {
     this.user = userService.getCurrentUser();
   }
 
@@ -31,5 +34,9 @@ export class AppComponent {
     this.userService.logout().subscribe(() => {
       this.user = of(null);
     });
+  }
+
+  search() {
+    this.router.navigate(['search', this.searchTxt]);
   }
 }
--- src/app/items/items-page/items-page.component.ts
+++ src/app/items/items-page/items-page.component.ts
@@ -3,31 +3,26 @@ import { ItemsService } from '../items.service';
 import { Item } from '../items.model';
 import { Observable, of } from 'rxjs';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-items-page',
-  imports: [CommonModule, RouterLink, MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './items-page.component.html',
   styleUrl: './items-page.component.css'
 })
 export class ItemsPageComponent {
   public static Route = {
-    path: 'items',
+    path: 'search/:keywords',
     title: 'Items',
     component: ItemsPageComponent
   }
 
   public items: Observable<Item[]> = of([]);
-  public searchTxt = "";
 
-  constructor(protected itemsService: ItemsService) {}
-
-  onSearchClick(): void {
-    this.items = this.itemsService.searchItems(this.searchTxt);
+  constructor(private route: ActivatedRoute, protected itemsService: ItemsService) {
+    this.route.params.subscribe(params => {
+      this.items = this.itemsService.searchItems(params['keywords']);
+    })
   }
 }
